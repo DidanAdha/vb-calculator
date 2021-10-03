f73b1e576a38036661c253eeaf756bf38e520fe7
Didan Adha <didanadha99@gmail.com>
2021-10-03 08:56:39 +0700

    add File

---
+++ Calculator/Form1.Designer.vb
@@ -0,0 +1,284 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.labelTemp2 = New System.Windows.Forms.Label()
+        Me.labelOpt = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btnMin = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.btnResult = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
+        Me.btnDel = New System.Windows.Forms.Button()
+        Me.btnBagi = New System.Windows.Forms.Button()
+        Me.btnKali = New System.Windows.Forms.Button()
+        Me.labelTemp = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'labelTemp2
+        '
+        Me.labelTemp2.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.labelTemp2.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.labelTemp2.Location = New System.Drawing.Point(68, 16)
+        Me.labelTemp2.Name = "labelTemp2"
+        Me.labelTemp2.Size = New System.Drawing.Size(162, 32)
+        Me.labelTemp2.TabIndex = 1
+        Me.labelTemp2.Text = "0"
+        Me.labelTemp2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'labelOpt
+        '
+        Me.labelOpt.AutoSize = True
+        Me.labelOpt.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.labelOpt.Location = New System.Drawing.Point(12, 52)
+        Me.labelOpt.Name = "labelOpt"
+        Me.labelOpt.Size = New System.Drawing.Size(27, 32)
+        Me.labelOpt.TabIndex = 2
+        Me.labelOpt.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 152)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(50, 50)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "7"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(68, 152)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(50, 50)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "8"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(124, 152)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(50, 50)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "9"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'btnMin
+        '
+        Me.btnMin.Location = New System.Drawing.Point(180, 152)
+        Me.btnMin.Name = "btnMin"
+        Me.btnMin.Size = New System.Drawing.Size(50, 50)
+        Me.btnMin.TabIndex = 6
+        Me.btnMin.Text = "-"
+        Me.btnMin.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 208)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(50, 50)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "4"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(68, 208)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(50, 50)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "5"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(124, 208)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(50, 50)
+        Me.Button7.TabIndex = 9
+        Me.Button7.Text = "6"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'btnPlus
+        '
+        Me.btnPlus.Location = New System.Drawing.Point(180, 208)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(50, 50)
+        Me.btnPlus.TabIndex = 10
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(12, 264)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(50, 50)
+        Me.Button9.TabIndex = 11
+        Me.Button9.Text = "1"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(68, 264)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(50, 50)
+        Me.Button10.TabIndex = 12
+        Me.Button10.Text = "2"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(124, 264)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(50, 50)
+        Me.Button11.TabIndex = 13
+        Me.Button11.Text = "3"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(12, 320)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(162, 50)
+        Me.Button13.TabIndex = 15
+        Me.Button13.Text = "0"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'btnResult
+        '
+        Me.btnResult.Location = New System.Drawing.Point(180, 264)
+        Me.btnResult.Name = "btnResult"
+        Me.btnResult.Size = New System.Drawing.Size(50, 106)
+        Me.btnResult.TabIndex = 18
+        Me.btnResult.Text = "="
+        Me.btnResult.UseVisualStyleBackColor = True
+        '
+        'btnC
+        '
+        Me.btnC.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnC.Location = New System.Drawing.Point(12, 96)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(50, 50)
+        Me.btnC.TabIndex = 19
+        Me.btnC.Text = "C"
+        Me.btnC.UseVisualStyleBackColor = True
+        '
+        'btnDel
+        '
+        Me.btnDel.Location = New System.Drawing.Point(68, 96)
+        Me.btnDel.Name = "btnDel"
+        Me.btnDel.Size = New System.Drawing.Size(50, 50)
+        Me.btnDel.TabIndex = 20
+        Me.btnDel.Text = "Del"
+        Me.btnDel.UseVisualStyleBackColor = True
+        '
+        'btnBagi
+        '
+        Me.btnBagi.Location = New System.Drawing.Point(124, 96)
+        Me.btnBagi.Name = "btnBagi"
+        Me.btnBagi.Size = New System.Drawing.Size(50, 50)
+        Me.btnBagi.TabIndex = 21
+        Me.btnBagi.Text = "/"
+        Me.btnBagi.UseVisualStyleBackColor = True
+        '
+        'btnKali
+        '
+        Me.btnKali.Location = New System.Drawing.Point(180, 96)
+        Me.btnKali.Name = "btnKali"
+        Me.btnKali.Size = New System.Drawing.Size(50, 50)
+        Me.btnKali.TabIndex = 22
+        Me.btnKali.Text = "X"
+        Me.btnKali.UseVisualStyleBackColor = True
+        '
+        'labelTemp
+        '
+        Me.labelTemp.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.labelTemp.Font = New System.Drawing.Font("Segoe UI", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.labelTemp.Location = New System.Drawing.Point(68, 52)
+        Me.labelTemp.Name = "labelTemp"
+        Me.labelTemp.Size = New System.Drawing.Size(162, 32)
+        Me.labelTemp.TabIndex = 23
+        Me.labelTemp.Text = "0"
+        Me.labelTemp.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(245, 382)
+        Me.Controls.Add(Me.labelTemp)
+        Me.Controls.Add(Me.btnKali)
+        Me.Controls.Add(Me.btnBagi)
+        Me.Controls.Add(Me.btnDel)
+        Me.Controls.Add(Me.btnC)
+        Me.Controls.Add(Me.btnResult)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.btnMin)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.labelOpt)
+        Me.Controls.Add(Me.labelTemp2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents labelTemp2 As Label
+    Friend WithEvents labelOpt As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents btnMin As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents btnResult As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btnDel As Button
+    Friend WithEvents btnBagi As Button
+    Friend WithEvents btnKali As Button
+    Friend WithEvents labelTemp As Label
+End Class
---
+++ Calculator/Form1.vb
@@ -0,0 +1,128 @@
+﻿Public Class Form1
+    Dim opt As String
+    Dim result As Integer
+    Dim tmp As Integer?
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        setLabelTemp(Button13.Text)
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        setLabelTemp(Button9.Text)
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        setLabelTemp(Button10.Text)
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        setLabelTemp(Button11.Text)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        setLabelTemp(Button5.Text)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        setLabelTemp(Button6.Text)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        setLabelTemp(Button7.Text)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        setLabelTemp(Button1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        setLabelTemp(Button2.Text)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        setLabelTemp(Button3.Text)
+    End Sub
+
+    Private Sub setLabelTemp(s As String)
+        If labelTemp.Text = "0" Then
+            labelTemp.Text = s
+        Else
+            labelTemp.Text = labelTemp.Text & s
+        End If
+    End Sub
+
+    Private Sub btnDel_Click(sender As Object, e As EventArgs) Handles btnDel.Click
+        Dim s As String
+        s = labelTemp.Text
+        s = s.Substring(0, s.Length - 1)
+        If s = "" Then
+            labelTemp.Text = "0"
+        Else
+            labelTemp.Text = s
+        End If
+
+    End Sub
+
+    Private Sub setOpt(s As String)
+
+        If tmp Is Nothing Then
+            opt = s
+            labelOpt.Text = s
+            labelTemp2.Text = labelTemp.Text
+            labelTemp.Text = "0"
+            tmp = Integer.Parse(labelTemp2.Text)
+        Else
+            countResult()
+            opt = s
+            labelOpt.Text = s
+        End If
+    End Sub
+
+    Private Sub countResult()
+        If opt = "/" Then
+            result = tmp / Integer.Parse(labelTemp.Text)
+        ElseIf opt = "X" Then
+            result = tmp * Integer.Parse(labelTemp.Text)
+        ElseIf opt = "-" Then
+            result = tmp - Integer.Parse(labelTemp.Text)
+        ElseIf opt = "+" Then
+            result = tmp + Integer.Parse(labelTemp.Text)
+        End If
+        labelTemp.Text = "0"
+        labelTemp2.Text = result.ToString
+        tmp = result
+    End Sub
+
+    Private Sub btnBagi_Click(sender As Object, e As EventArgs) Handles btnBagi.Click
+        setOpt(btnBagi.Text)
+    End Sub
+
+    Private Sub btnKali_Click(sender As Object, e As EventArgs) Handles btnKali.Click
+        setOpt(btnKali.Text)
+    End Sub
+
+    Private Sub btnMin_Click(sender As Object, e As EventArgs) Handles btnMin.Click
+        setOpt(btnMin.Text)
+    End Sub
+
+    Private Sub btnPlus_Click(sender As Object, e As EventArgs) Handles btnPlus.Click
+        setOpt(btnPlus.Text)
+    End Sub
+
+    Private Sub btnResult_Click(sender As Object, e As EventArgs) Handles btnResult.Click
+        countResult()
+    End Sub
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+        labelOpt.Text = ""
+        labelTemp.Text = "0"
+        labelTemp2.Text = "0"
+        opt = Nothing
+        result = Nothing
+        tmp = Nothing
+
+    End Sub
+End Class
